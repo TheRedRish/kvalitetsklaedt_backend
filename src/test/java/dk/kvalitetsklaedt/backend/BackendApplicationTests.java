0d9980e30b3363b8ac5b1f4c8d0b214537162a07
Rune Hansen <rune@roeddik.net>
2025-05-06 11:04:54 +0200

    Initial commit

---
+++ src/test/java/dk/kvalitetsklaedt/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kvalitetsklaedt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
