a059076cdd510bd17aff54bdc0c3f68f0e31eeec
Rune <roeddik@live.dk>
2025-05-07 11:11:45 +0200

    Removed cors from controller

--- src/main/java/dk/kvalitetsklaedt/backend/controller/SubscriptionController.java
+++ src/main/java/dk/kvalitetsklaedt/backend/controller/SubscriptionController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/subscriptions")
-@CrossOrigin // allow frontend dev from localhost
 public class SubscriptionController {
 
     private final SubscriptionRepository repo;
