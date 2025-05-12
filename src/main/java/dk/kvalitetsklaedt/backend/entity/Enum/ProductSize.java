7b78e37c9c7c47d815564b1db6dd723beaea45c5
MadsLindersson <Mali0002@stud.kea.dk>
2025-05-12 13:55:39 +0200

    Initial push of entity classes with JPA database setup. Added service package.

---
+++ src/main/java/dk/kvalitetsklaedt/backend/entity/Enum/ProductSize.java
@@ -0,0 +1,10 @@
+package dk.kvalitetsklaedt.backend.entity.Enum;
+
+public enum ProductSize {
+    S,
+    M,
+    L,
+    Xl,
+    XXL,
+    XXXL
+}
---
+++ src/main/java/dk/kvalitetsklaedt/backend/entity/Enum/ProductTag.java
@@ -0,0 +1,9 @@
+package dk.kvalitetsklaedt.backend.entity.Enum;
+
+public enum ProductTag {
+    SUMMER,
+    WINTER,
+    SPRING,
+    AUTUMN,
+    ALL_YEAR
+}
