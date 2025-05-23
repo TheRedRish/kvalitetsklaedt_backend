b35b88b47c62fb4763a9e2cb1375b6490b92e2b1
bikstok <jkjaerulff@gmail.com>
2025-05-23 10:01:43 +0200

    Init data for products

---
+++ src/main/java/dk/kvalitetsklaedt/backend/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package dk.kvalitetsklaedt.backend.repository;
+
+import dk.kvalitetsklaedt.backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
+
